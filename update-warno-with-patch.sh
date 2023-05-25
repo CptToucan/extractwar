ddfb675e8bd9e89f48de5063dd67f8bb9e0ce313
Toucan <autotoucan@gmail.com>
2023-05-25 07:03:44 +0100

    feat: add update with patch sh

---
+++ update-warno-with-patch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+patch_name=$1  # Parameter for input file
+
+if [ -z "$1" ]; then
+  echo "Error: No patch name provided."
+  echo "Usage: $0 <patch_file>"
+  exit 1
+fi
+
+./bin/dev generate-ndf-toucan ./read/warno ./read-previous-patch/warno --modDir ../../warno-ndf-data/warno
+./bin/dev ndf-to-json ./read/warno warno.json ./read-previous-patch/warno
+./bin/dev upload-firebase-bundle warno.json warno/units-and-divisions.json
+./bin/dev add-patch-to-firebase "$patch_name" patch.json
